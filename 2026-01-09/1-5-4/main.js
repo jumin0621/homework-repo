172af693fe1dfd7aa62ea868b2ac627dae1023f3
jumin0621 <punima0477@gmail.com>
2026-01-09 17:35:43 +0900

    1-5-3, 1-5-4

---
+++ 2026-01-09/1-5-4/main.js
@@ -0,0 +1,7 @@
+import { add, sub } from "./math.js";
+import userProfile from "./userProfile.js";
+
+console.log("math add =", add(10, 20));
+console.log("math sub =", sub(20, 10));
+console.log("이름:", userProfile.name);
+console.log("나이:", userProfile.age);
